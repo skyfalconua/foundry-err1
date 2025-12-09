3d93ec58214237ea13710275b7797ed5dbd4d0ca
Andrew Sokolov <artas90@gmail.com>
2025-12-09 22:30:02 +0200

    mcp test

--- server.py
+++ server.py
@@ -2,11 +2,10 @@
 
 import os
 
-from agent_framework import ChatAgent
+from agent_framework import AIFunction, ChatAgent, ChatClientProtocol, MCPStreamableHTTPTool, ai_function
 from agent_framework_ag_ui import add_agent_framework_fastapi_endpoint
 from agent_framework.azure import AzureOpenAIChatClient
 from azure.core.credentials import AccessToken, TokenCredential
-from azure.identity import AzureCliCredential
 from fastapi import FastAPI
 import time
 
@@ -46,18 +45,30 @@ if not endpoint:
 if not deployment_name:
   raise ValueError("AZURE_OPENAI_DEPLOYMENT_NAME environment variable is required")
 
+
+# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- - -
+
+mcp_server = MCPStreamableHTTPTool(
+  name="jira",
+  url="http://0.0.0.0:9000/sse",
+  load_tools=True,
+  load_prompts=True
+)
+
 chat_client = AzureOpenAIChatClient(
   credential=get_credentials(),
   endpoint=endpoint,
   deployment_name=deployment_name,
 )
 
-# Create the AI agent
 agent = ChatAgent(
   name="AGUIAssistant",
   instructions="You are a helpful assistant.",
   chat_client=chat_client,
+  tools=mcp_server.functions,
 )
+
+# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- - -
 
 # Create FastAPI app
 app = FastAPI(title="AG-UI Server")
